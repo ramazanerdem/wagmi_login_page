30076f8f468f55e79ecd86ac7b62c82a8831b7c2
bugrahan gulmez <bugrahan1645@gmail.com>
2023-03-30 20:48:36 +0300

    formik eklendi

--- src/pages/components/InForm.jsx
+++ src/pages/components/InForm.jsx
@@ -1,20 +1,39 @@
+import {Formik} from "formik";
+import * as Yup from "yup";
+
 const InForm = ({ passwordShown, setPasswordShown }) => {
+
+  const handleSubmitForm = (values) => {
+    console.log(values)
+  }
   return (
-    <>
-      <form>
+
+    <Formik
+      initialValues={{email:"", password:""}}
+      onSubmit={handleSubmitForm}
+      validationSchema={validationSchema}
+    >
+      {
+        ({values, touched, handleChange, handleSubmit, errors})=>(
+          <form onSubmit={handleSubmit}>
         <p className="input-header">Email</p>
-        <input type="email" placeholder="mail@website.com" />
+        <input name="email" onChange={handleChange} type="email" placeholder="mail@website.com" />
+        {errors.email && touched.email ? <p className="input-header">{errors.email}</p>: null}
+
         <p className="input-header">Enter your password</p>
         <div className="pass-box">
           <input
+            name="password"
             type={passwordShown ? 'text' : 'password'}
             placeholder="min 8 character"
+            onChange={handleChange}
           />
           <i
             class={`fa-regular ${!passwordShown ? 'fa-eye' : 'fa-eye-slash'}`}
             onClick={() => setPasswordShown(!passwordShown)}
           ></i>
         </div>
+        {errors.password && touched.password ? <p className="input-header">{errors.password}</p>: null}
 
         <div className="form-check">
           <div className="left">
@@ -26,11 +45,23 @@ const InForm = ({ passwordShown, setPasswordShown }) => {
           </div>
         </div>
 
-        <button type="submit" className="submit-button">
+        <button onClick={handleSubmit} type="submit" className="submit-button">
           Sign In
         </button>
       </form>
-    </>
+        )
+      }
+    </Formik>
+
   )
 }
 export default InForm
+
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email("Lütfen geçerli bir email adresi girin")
+    .required("Email"),
+  password: Yup.string()
+    .required("Şifre girmek zorunludur")
+    .min(8,"Şifreniz en az 8 karakterli olmalıdır")
+})
